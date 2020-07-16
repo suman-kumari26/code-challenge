cfa2cbcdc6cf25273c5d90f491f3355d1ee130f8
Suman Kumari <suman@synup.com>
2020-07-16 12:38:29 +0530

    added validation for company email address domain

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,15 @@
 class Company < ApplicationRecord
   has_rich_text :description
 
+  #validations
+  validates :name, :zip_code, presence: true
+  validate :is_email_domain_valid?
 
+  private
+
+  def is_email_domain_valid?
+    if email.present? && !(email.ends_with? "@getmainstreet.com")
+      errors.add(:email, "address is not valid")
+    end
+  end
 end
