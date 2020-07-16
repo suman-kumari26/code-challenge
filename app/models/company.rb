7dcf33c18f29dfb3af5437d8823a8fb562fcda52
Suman Kumari <suman@synup.com>
2020-07-16 12:42:25 +0530

    added code to set address before save and show the city and state in company show page

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,7 +5,21 @@ class Company < ApplicationRecord
   validates :name, :zip_code, presence: true
   validate :is_email_domain_valid?
 
+  #callbacks
+  before_save :set_address
+
+  def self.fetch_address(zip_code)
+    return {} unless zip_code.present?
+    address_hash = ZipCodes.identify(zip_code)
+  end
+
   private
+
+  def set_address
+    address_hash = Company.fetch_address(zip_code)
+    self.state = address_hash[:state_code]
+    self.city = address_hash[:city]
+  end
 
   def is_email_domain_valid?
     if email.present? && !(email.ends_with? "@getmainstreet.com")
